2ae0d371fbeac9141afddf63c87bee46bb8c195a
Richard Wellum <richard@richardwellum.com>
2015-08-03 12:09:58 +0100

    Initial

---
+++ LastSundayInMonth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LastSundayInMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sundays14 = LastDaysInMonthForYear(2014, DayOfWeek.Sunday).ToArray();
+            var saturdays13 = LastDaysInMonthForYear(2013, DayOfWeek.Saturday).ToArray();
+            var tuesdays99 = LastDaysInMonthForYear(1999, DayOfWeek.Tuesday).ToArray();
+            var tuesdays00 = LastDaysInMonthForYear(2000, DayOfWeek.Tuesday).ToArray();
+        }
+
+        private static IEnumerable<DateTime> LastDaysInMonthForYear(int y, DayOfWeek w)
+        {
+            return
+                Enumerable.Range(1, 12)
+                    .Select(i => new DateTime(y, i, 1).AddMonths(1).AddDays(-(int) w - 1))
+                    .Select(d => d.AddDays((int) w - (int) d.DayOfWeek));
+        } 
+    }
+}
